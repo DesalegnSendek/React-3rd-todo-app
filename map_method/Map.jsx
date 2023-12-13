f5ccec699fb1e8dfc274c7b5dab2df2eb09557e9
DesalegnSendek1 <118896834+DesalegnSendek@users.noreply.github.com>
2023-12-13 13:40:30 +0300

    Update Map.jsx

--- map_method/Map.jsx
+++ map_method/Map.jsx
@@ -1,8 +1,17 @@
+//array of objects
 const products = [
   { id: 1, name: 'Shirt', price: 20 },
   { id: 2, name: 'Pants', price: 30 },
   { id: 3, name: 'Shoes', price: 50 },
 ];
+
+const animals = [
+  { id: 1, name: 'Dog', canFly: false },
+  { id: 2, name: 'Bird', canFly: true },
+  { id: 3, name: 'Fish', canFly: false },
+];
+
+const fruits = ['apple', 'banana', 'orange'];
 
 function Product(props) {
   const { name, price } = props;
@@ -14,16 +23,43 @@ function Product(props) {
   );
 }
 
-function Map() {
+function Animal(props) {
+  const { name, canFly } = props;
   return (
     <div>
-      <h2>Products</h2>
+      <h3>{name}</h3>
+      {canFly && <p>This animal can fly.</p>}
+    </div>
+  );
+}
+
+
+
+
+function Map() {
+  return (
+    <><div>
+      <h1>Products</h1>
       {products.map((product) => (
         <Product key={product.id} name={product.name} price={product.price} />
       ))}
-
-      
     </div>
+    
+    <div>
+      <h1>Animals</h1>
+      {animals.map((animal) => (
+        <Animal key={animal.id} name={animal.name} canFly={animal.canFly} />
+      ))}
+    </div>
+
+    <div>
+    <h1>Fruits</h1>
+    <ul>
+      {fruits.map((fruit) => (
+        <li key={fruit}>{fruit}</li>
+      ))}
+    </ul></div>
+    </>
   );
 }
 
