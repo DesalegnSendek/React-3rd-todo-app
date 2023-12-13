b49d1ce21a54c2ff5e1cf772dcd83ed607baf48a
DesalegnSendek1 <118896834+DesalegnSendek@users.noreply.github.com>
2023-12-13 13:32:36 +0300

    Update Map.jsx

--- map_method/Map.jsx
+++ map_method/Map.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+const products = [
+  { id: 1, name: 'Shirt', price: 20 },
+  { id: 2, name: 'Pants', price: 30 },
+  { id: 3, name: 'Shoes', price: 50 },
+];
 
-const Map = () => {
-  
-const fruits = ['apple', 'banana', 'orange'];
+function Product(props) {
+  const { name, price } = props;
+  return (
+    <div>
+      <h3>{name}</h3>
+      <p>Price: ${price}</p>
+    </div>
+  );
+}
 
 function Map() {
   return (
-    <ul>
-      {fruits.map((fruit) => (
-        <li key={fruit}>{fruit}</li>
+    <div>
+      <h2>Products</h2>
+      {products.map((product) => (
+        <Product key={product.id} name={product.name} price={product.price} />
       ))}
-    </ul>
+
+      
+    </div>
   );
 }
-} 
 
-export default Map
+export default Map;
